5d837cee864a3c57ae5e38ea1f0c8f73a34ce182
suneel281275 <varma8553@gmail.com>
2016-11-22 14:43:40 +0530

    comments

--- SpringRestCountry/src/main/java/com/vminds/controller/SpringRestController.java
+++ SpringRestCountry/src/main/java/com/vminds/controller/SpringRestController.java
@@ -71,6 +71,7 @@ public class SpringRestController {
 	Status modifyEntity(@RequestBody Customer customer) {
 		try{
 		dataServices.modifyEntity(customer);
+		//gfgfgfgffgfg
 		return new Status(1, "Employee updated Successfully !");
 		}catch(Exception e){
 			return new Status(0, e.toString());
